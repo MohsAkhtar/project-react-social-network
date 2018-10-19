5b57fbba45e3b00ea97b21f4787532b39d6f728a
MohsAkhtar <mohs.akhtar@gmail.com>
2018-10-19 10:52:38 +0100

    Added ability to register users to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,55 @@
 // file to do with all login, authentication, passport etc.
 const express = require('express'); // to use router we need to bring in express first
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users works' })); // res.json is similar to res.send except it is going to output json
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  // Check if email is already registered
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already registered' });
+    } else {
+      // retrieve avatar from email using gravatar
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default image
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      // Encrypt password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          // Save user to db
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 // we have to export router in order for the server.js file to pick it up
 module.exports = router;
