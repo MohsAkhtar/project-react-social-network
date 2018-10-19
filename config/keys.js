2b357caf7106d7b7b8c6ba13ec74fa2be45dd6d5
MohsAkhtar <mohs.akhtar@gmail.com>
2018-10-19 10:08:10 +0100

    Added config for mongodb

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+//module.exports makes object available outside of this file as we need to connect to it
+module.exports = {
+  mongoURI: 'mongodb://mohs:mohs1234@ds135653.mlab.com:35653/mern-db'
+};
