e17ec11bd830001ee3f70ae08e979a9db8e46ac4
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-15 16:19:48 +0000

    Added ability to comment and delete your comment off a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -191,4 +191,38 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access  Private
+router.post(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check if comment exists
+        if (
+          post.comment.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: 'Comment does not exist' });
+        }
+
+        // Get remove index
+        const removeIndex = post.comment
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
