8adc0edb1437f91f7a67ab2f5bde903203483506
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-15 15:00:41 +0000

    Added schema for posts and validation

---
+++ validation/post.js
@@ -0,0 +1,25 @@
+// Validation for post
+const isEmpty = require('./is-empty');
+const validator = require('validator');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+  // Post Validations
+
+  if (!validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  if (validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  // return object with any errors
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
