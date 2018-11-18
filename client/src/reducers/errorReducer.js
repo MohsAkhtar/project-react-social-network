de4821f18417da9efc74d6071f3928eb6f62bf82
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-18 20:03:41 +0000

    Added a reducer to clear errors

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,6 +1,6 @@
 // Reducer here is for all errors across application
 
-import { GET_ERRORS } from '../actions/types';
+import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types';
 
 // set to empty object as we won't initially have any errors
 const initialState = {};
@@ -9,6 +9,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload; // just want to return errors
+    case CLEAR_ERRORS:
+      return {};
     default:
       return state;
   }
